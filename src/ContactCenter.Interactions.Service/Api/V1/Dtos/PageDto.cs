015ac688a8f6b1a59e656a345576dabc05d686cb
= <=>
2025-07-14 13:11:29 -0400

    pagedto

---
+++ src/ContactCenter.Interactions.Service/Api/V1/Dtos/PageDto.cs
@@ -0,0 +1,19 @@
+namespace ServiceTitan.ContactCenter.Interactions.Service.Api.V1.Dtos;
+
+public record PageDto
+{
+    public required int PageNumber { get; set; }
+    public required int PageSize { get; set; }
+    public required int TotalCount { get; set; }
+    public required int TotalPages { get; set; }
+    public required OrderOutputDto[] Items { get; set; }
+
+    public PageDto(int pageNumber, int totalPages, int pageSize, int totalCount, T[] items)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+        TotalPages = totalPages;
+        Items = items;
+    }
+}
